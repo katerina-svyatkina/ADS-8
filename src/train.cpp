a0ec5eb0cd121410952b0c3508bd2a8f02df35da
Екатерина Святкина <99070196+katerina-svyatkina@users.noreply.github.com>
2022-05-08 08:05:00 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,40 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include <iostream>
+void Train::addCage(bool light) {
+    Cage* temp = create(light);
+    if (isEmpty()) {
+       current = first = tail = temp;
+    } else {
+        tail->next = temp;
+        tail->next->prev = tail;
+        tail = tail->next;
+        first->prev = temp;
+        tail->next = first;
+    }
+
+}
+int Train::getLength() {
+    if (current == first && !first->light)
+            first->light = true;
+    while (true) {
+        int len = 0, temp = 0;
+        do {
+            len++;
+            countOp++;
+            current = current->next;
+        } while (!current->light);
+        current->light = false;
+        temp = len;
+        while (len--) {
+            countOp++;
+            current = current->prev;
+        }
+        if (!current->light) {
+            return temp;
+        }
+    }
+}
+int Train::getOpCount() {
+    return countOp;
+}
